c237acebd67427a566c790afbf18118b2dc8fc80
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-03-21 19:09:36 +0100

    feat: routing header created and basic design of header, home and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import { Header } from './common/Header/Header'
 import { Body } from './pages/Body/Body'
 
 
 
 function App() {
- 
+
   return (
     <>
-       <Body />
+      <Header />
+      <Body />
     </>
   )
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-
+import "./Register.css"
 
 
 
