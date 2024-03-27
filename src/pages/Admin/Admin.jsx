3b425504d69e55b9272d81adb216de747c9eebf1
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-03-27 14:38:09 +0100

    feat: view admin created

---
+++ src/pages/Admin/Admin.jsx
@@ -0,0 +1,14 @@
+import "./Admin.css"
+import { Header } from "../../common/Header/Header"
+
+export const Admin = () => {
+
+    return (
+        <>
+            <Header />
+            <div className="adminDesign">
+                ADMIN
+            </div>
+        </>
+    )
+}
